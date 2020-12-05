bd2528f23dec6dd788292f458a9663a4d91401d0
issam22009 <56225133+issam22009@users.noreply.github.com>
2020-12-04 20:43:51 -0500

    Create Client.java

---
+++ etape 2_/Client.java
@@ -0,0 +1,31 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Socket s = new Socket("localhost",1234);
+			InputStream is = s.getInputStream();
+			OutputStream os = s.getOutputStream();
+			Scanner scanner = new Scanner(System.in);
+			
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
